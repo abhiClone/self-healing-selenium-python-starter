2a1b3f9104278a025c98a3a0a77ff4d90d4e2e19
abhiClone <krabhishek543@gmail.com>
2025-09-01 16:07:28 +0200

    Update test_login_working.py

--- tests/test_login_working.py
+++ tests/test_login_working.py
@@ -1,6 +1,6 @@
 from src.pages.login_page import LoginPage
 
-def test_login_normal(driver, base_url):
+def test_login_valid(driver, base_url):
     page = LoginPage(driver)
     page.goto(base_url, broken=False)
     page.fill_username("demo")
