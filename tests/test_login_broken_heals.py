43071757b3e0b0782ddab0659ead93f1e2fed73f
abhiClone <krabhishek543@gmail.com>
2025-09-01 16:08:42 +0200

    Update test_login_broken_heals.py

--- tests/test_login_broken_heals.py
+++ tests/test_login_broken_heals.py
@@ -1,6 +1,6 @@
 from src.pages.login_page import LoginPage
 
-def test_login_broken_heals(driver, base_url):
+def test_login_valid(driver, base_url):
     page = LoginPage(driver)
     page.goto(base_url, broken=True)
     page.fill_username("demo")
